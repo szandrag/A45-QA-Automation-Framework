d60ac54efb45e86e7d270767c95d438e15bb13d8
szandragecse <szandra.gecse@testpro.io>
2023-04-24 00:31:35 -0400

    Homework#15
    The URL has to be changed from https://bbb.testpro.io to https://testpro.io

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -88,7 +88,7 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://bbb.testpro.io/";
+        String url = "https://testpro.io/";
         driver.get(url);
 
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
